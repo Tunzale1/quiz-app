c7a547c5c39a00c3cd040ed47c94a5cf3cb171ce
Tünzalə <114104944+Tunzale1@users.noreply.github.com>
2023-04-09 18:37:52 +0400

    sehv duz score part

--- quiz app/game.js
+++ quiz app/game.js
@@ -1,13 +1,15 @@
-const question=document.getElementById("question");
-const choices= Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText= document.getElementById("questionCounter");
+const scoreText = document.getElementById("score")
 
-let currentQuestion={};
-let acceptingAnswers= true;
-let score=0;
-let questionCounter=0;
-let availableQuestions=[];
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
-let questions=[
+let questions = [
     {
         question: "Menim en sevdiyim reng hansidir?",
         choice1:"qirmizi",
@@ -35,49 +37,78 @@ let questions=[
         answer: 4
 
     },
+];
 
-]
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
-//hahha
-const CORRECT_BONUS=10;
-const MAX_QUESTIONS=3;
-
-startGame=()=>{
-    questionCounter=0;
-    score=0;
-    availableQuestions=[...questions];
-    console.log(availableQuestions);
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
     getNewQuestion();
 };
 
-getNewQuestion=()=>{
-    if(availableQuestions.length=0 || questionCounter >= MAX_QUESTIONS){
-       
-        return window.location.assign("./end.html")
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
     }
     questionCounter++;
-    const questionIndex= Math.floor(Math.random() *availableQuestions.length);
-    currentQuestion=availableQuestions[questionIndex];
-    question.innerText=currentQuestion.question;
+    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
 
-choices.forEach(choice=>{
-    const number= choice.dataset['number'];
-    choice.innerText=currentQuestion["choice"+ number]
+
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = 
+        selectedAnswer == currentQuestion.answer ? "duz" : "sehv";
+        
+        if(classToApply === "duz"){
+            incrementScore(CORRECT_BONUS);
+        }
+        if(classToApply==="sehv"){
+            decrementScore(CORRECT_BONUS)
+        }
+        selectedChoice.parentElement.classList.add(classToApply)
+       
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+          }, 1000);
+        
+      
+    });
 });
-availableQuestions.splice(questionIndex, 1);
-console.log(availableQuestions);
-acceptingAnswers=true;
-}
 
-choices.forEach(choice=>{
-    choice.addEventListener("click", e=>{
-        if(!acceptingAnswers) return;
-        acceptingAnswers= false;
-        const selectedChoice=e.target;
-        const selectedAnswer = selectedChoice.dataset["number"]
-        console.log(selectedAnswer);
-        getNewQuestion()
-    })
-})
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
 
-startGame()
+decrementScore = num => {
+    score -= num;
+    scoreText.innerText= score
+}  
+
+startGame();
